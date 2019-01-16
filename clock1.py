f73ddbfcdbc1e4d216fb7d47dce0eccecf4c22d4
pranitasalunke <46518349+pranitasalunke@users.noreply.github.com>
2019-01-16 14:01:41 +0530

    Add files via upload

---
+++ clock1.py
@@ -0,0 +1,4 @@
+hour=int(input('enter the hours shown on the clock :'))
+min=int(input('enter the minutes shown in the clock :'))
+angle=(hour*30)+(min*0.5)
+print('The angle made by the hour hand with the vertical is :',angle,'degrees')
